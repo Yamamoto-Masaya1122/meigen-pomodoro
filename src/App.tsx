70951cb3cdd807172209beab536772a0d9c10b01
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-08-28 01:36:47 +0900

    不要なモジュールのimportを削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button, HStack } from "@chakra-ui/react"
-
 
 function App() {
   const [count, setCount] = useState(0)
