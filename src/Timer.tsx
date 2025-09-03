5c83d57736aaba9c70174b01cf34c9820dfd347c
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-09-03 20:40:18 +0900

    トップページのUI修正

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function Timer() {
+  const [time, setTime] = useState(25);
   return (
     <>
       <h1>タイマーページです</h1>
+      <p>{time}</p>
     </>
   )
 }
--- src/Top.tsx
+++ src/Top.tsx
@@ -4,10 +4,11 @@ import "./Top.css"
 export default function Top() {
   return (
     <>
-      <section className="section">
-        <h1>名言ポモドーロ</h1>
-        <Button bg="cyan.500" _hover={{ bg: "cyan.600" }} size="xl">一歩踏みだす</Button>
-      </section>
+      <div className="container">
+        <h1>MEI-POMO</h1>
+        <p>著名人が残した名言を見て気持ちを高めることができるポモドーロタイマーです</p>
+        <Button bg="cyan.500" _hover={{ bg: "cyan.600" }} size="xl">はじめる</Button>
+      </div>
     </>
   )
 }
