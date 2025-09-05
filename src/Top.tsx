93fc5cd4eae1e58f5300c8dee62b1e6717db5e3c
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-09-06 01:11:03 +0900

    はじめるボタン修正

--- src/Top.tsx
+++ src/Top.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@chakra-ui/react"
 import { useNavigate } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 import "./Top.css"
 
 export default function Top() {
@@ -10,8 +11,8 @@ export default function Top() {
       <div className="container">
         <h1>MEI-POMO</h1>
         <p>著名人が残した名言によってやる気を高めるポモドーロタイマーです</p>
-        <Button onClick={() => navigate("/timer")} bg="cyan.500" _hover={{ bg: "cyan.600" }} size="xl">
-          はじめる
+        <Button onClick={() => navigate("/timer")} bg="gray.500" _hover={{ bg: "gray.600" }} size="xl">
+          はじめる <FaArrowRightLong />
         </Button>
       </div>
     </>
