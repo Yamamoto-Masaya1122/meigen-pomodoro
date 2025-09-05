8627e6406ba802ab4556b625f1402fdf70f85810
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-09-06 01:06:00 +0900

    トップページとフォント体を調整

--- src/Top.tsx
+++ src/Top.tsx
@@ -9,7 +9,7 @@ export default function Top() {
     <>
       <div className="container">
         <h1>MEI-POMO</h1>
-        <p>著名人が残した名言を見て気持ちを高めることができるポモドーロタイマーです</p>
+        <p>著名人が残した名言によってやる気を高めるポモドーロタイマーです</p>
         <Button onClick={() => navigate("/timer")} bg="cyan.500" _hover={{ bg: "cyan.600" }} size="xl">
           はじめる
         </Button>
